1e609c107f14bc29459baff99716d2831b715f84
GitHub Action <action@github.com>
2021-12-12 17:16:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/procrastinationfighter/ideaannotator/MyBundle.kt
+++ src/main/kotlin/com/github/procrastinationfighter/ideaannotator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.procrastinationfighter.ideaannotator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/procrastinationfighter/ideaannotator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.procrastinationfighter.ideaannotator.services
+
+import com.github.procrastinationfighter.ideaannotator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
